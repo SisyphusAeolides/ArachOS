b179062360b905d4f676b1e95baa8d84abcb64ce
Kenny Glauner <SisyphusAeolides@pm.me>
2026-09-05 08:20:18 -0500

    boot: remove unused generic boot templates

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -10,7 +10,7 @@ install_dir="arch"
 buildmodes=('iso')
 bootmodes=('uefi.grub')
 # Arach Kernel is a Multiboot2 image; only the GRUB mode can load this boot
-# contract. The generic Syslinux and systemd-boot templates are not selected.
+# contract. No Syslinux or systemd-boot boot mode is declared.
 arch="x86_64"
 pacman_conf="${ARACHOS_PACMAN_CONF:-pacman.conf}"
 airootfs_image_type="squashfs"
